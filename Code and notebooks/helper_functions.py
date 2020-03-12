bec59f588089c540a2b3da92b7155a40cbed1947
Deeparth Gupta <dprthgpta@gmail.com>
2020-03-12 14:43:20 +0530

    Removed wavinfo.py

--- Code and notebooks/helper_functions.py
+++ Code and notebooks/helper_functions.py
@@ -15,3 +15,25 @@ def extract_MFCC(audio):
         return None
      
     return mfccs
+
+import struct
+import librosa
+
+class WavInfo:
+    
+    def get_info(self,file):
+
+        wave = open(file,"rb")
+        riff = wave.read(12)
+        fmt = wave.read(36)
+        
+        num_channels_string = fmt[10:12]
+        num_channels = struct.unpack('<H', num_channels_string)[0]
+
+        sample_rate_string = fmt[12:16]
+        sample_rate = struct.unpack("<I",sample_rate_string)[0]
+        
+        bit_depth_string = fmt[22:24]
+        bit_depth = struct.unpack("<H",bit_depth_string)[0]
+
+        return (num_channels, sample_rate, bit_depth)
